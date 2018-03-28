649a6ab8d32e84e60154599cebd3da2502c4e1dd
armani <mark.armanious@heyorca.com>
2018-03-28 11:18:50 -0230

    finished the post, topic creation and post editing

--- mark_env/flaskr/flaskr/flaskr.py
+++ mark_env/flaskr/flaskr/flaskr.py
@@ -51,42 +51,80 @@ def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
-@app.route('/')
-def show_entries():
-    db = get_db()
-    cur = db.execute('select title, text from entries order by id desc')
-    entries = cur.fetchall()
-    return render_template('show_entries.html', entries=entries)
 
-@app.route('/add', methods=['POST'])
-def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
+@app.route('/')
+def show_topics():
     db = get_db()
-    db.execute('insert into entries (title, text) values (?, ?)',
-                 [request.form['title'], request.form['text']])
+    cur = db.execute('select title from topics order by createdAt desc')
+    topics = cur.fetchall()
+    return render_template('show_topics.html', topics=topics)
+
+@app.route('/addtopic', methods=['POST'])
+def add_topic():
+    userId = "2"
+    title = request.form['title']
+    db = get_db()
+    db.execute('insert into topics (userId, title) values (?, ?)',
+                 [userId, title])
     db.commit()
     flash('New entry was successfully posted')
-    return redirect(url_for('show_entries'))
+    return redirect(url_for('show_topics'))
+@app.route('/topic/<topic>/addpost', methods=['POST'])
+def add_post(topic):
+    userId = "2"
+    content = request.form['content']
+    title = request.form['title']
+    print(content)
+    db = get_db()
+    db.execute('insert into posts (userId, title, content, topic) values (?, ?, ?, ?)',
+                 [userId, title, content, topic])
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_posts',topic=topic))
+@app.route('/post/<postId>/editpost', methods=['GET','POST'])
+def edit_post(postId):
+    db = get_db()
+    cur = db.execute('select title,content,topic from posts where id=?',[postId])    
+    post = cur.fetchone()
+    topic = post['topic']
+    print(post['content'])
+    if(request.method == 'GET'):
+    	return render_template('edit_post.html' ,post=post,postId=postId, topic=topic)	
+    elif(request.method == 'POST'): 
+	title = request.form['title']
+	content = request.form['content']
+        db.execute('update posts set title=?, content=? where id=?', [title,content,postId])
+	db.commit()
+	return redirect(url_for('show_posts', topic=topic))
 
+@app.route('/topic/<topic>',methods=['GET'])
+def show_posts(topic):
+    db = get_db()
+    userId = "1"
+    cur = db.execute('select title, content, userId, id from posts where topic=? order by createdAt desc', [topic])
+    posts = cur.fetchall()
+    return render_template('show_posts.html', posts=posts, topic = topic, userId=userId) 
+
+    
+    
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
+        if request.form['username'] != "nana":
             error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
+        elif request.form['password'] != "nana":
             error = 'Invalid password'
         else:
             session['logged_in'] = True
             flash('You were logged in')
-            return redirect(url_for('show_entries'))
+            return redirect(url_for('show_topics'))
     return render_template('login.html', error=error)
 
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
-    return redirect(url_for('show_entries'))
+    return redirect(url_for('show_topics'))
 
 
