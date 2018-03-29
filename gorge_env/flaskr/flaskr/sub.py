a992e5fabc1f9087accc219071a00d3f90aa8a9e
George Neonakis <gnan88@mun.ca>
2018-03-29 14:21:15 -0230

    updated sub module for interface with topics module

--- gorge_env/flaskr/flaskr/sub.py
+++ gorge_env/flaskr/flaskr/sub.py
@@ -2,61 +2,52 @@ from flaskr import *
 
 class Sub:
 
-    def get_user_id():
-        ''' Dummy function to receive the id of the active user '''
-        
-        return 0
+    def isSubscribed(user):
+        ''' Checks if a user is subscribed '''
 
-    def get_topic_id():
-        ''' Dummy function to receive the id of a topic '''
-        
-        return 0
+        db = get_db()
+        cur = db.execute('select from subs topicId where userId = (?)', user)
+        isSub = cur.fetchall()
+        return isSub
 
-    def create_subscription():
+    def create_subscription(user, topic):
         ''' Create a user subscription to a topic and add it to the database '''
         
-        user = get_user_id()
-        topic = get_topic_id()
         db = get_db()
-        db.execute('insert into subscriptions (userid, topicid, viewed) values (?, ?, ?)',
-                   user, topic, True)
+        db.execute('insert into subs (userId, topicId, notified) values (?, ?, ?)',
+                   user, topic, 1)
         db.commit()
         flash('Subscribed!')
-        return redirect(url_for('show_entries'))
+        return redirect(url_for('show_topics'))
 
-    def unsubscribe():
+    def unsubscribe(user, topic):
         ''' Remove a user subscription from a topic '''
 
-        user = get_user_id()
-        topic = get_topic_id()
         db = get_db()
-        db.execute('delete from subscriptions where userid = (?), topicid = (?)', user, topic)
+        db.execute('delete from subs where userId = (?), topicId = (?)', user, topic)
         db.commit()
+        flash('Unsubscribed!')
+        return redirect(url_for('show_topics'))
 
-    def show_notifications():
+    def show_notifications(user):
         ''' Show a user's notifications on all relevant topics. To be called every time entries are shown'''
         
-        user = get_user_id()
         db = get_db()
-        cur = db.execute('select topicid from subscriptions where userid = (?), viewed = (?)', user, False)
+        cur = db.execute('select topicId from subs where userId = (?), notified = (?)', user, 0)
         sslist = cur.fetchall()
-        #add code to check topicids against all ids in entries, highlight posts that match
+        return sslist
 
-    def view_notification():
+    def view_notification(user, topic):
         ''' Mark a notification as viewed. To be called when a user clicks on a notification'''
         
-        user = get_user_id()
-        topic = get_topic_id()
         db = get_db()
-        db.execute('update subscriptions set viewed = (?) where userid = (?), topicid = (?)', True, user, topic)
+        db.execute('update subs set notified = (?) where userId = (?), topicId = (?)', 1, user, topic)
         db.commit()
-        return redirect(url_for('show_entries'))
 
-    def update_subscription():
+    def update_subscription(topic):
         ''' Update all subscriptions when a new post is made within a topic '''
         
-        topic = get_topic_id()
         db = get_db()
-        db.execute('update subscriptions set viewed = False where topicid = (?)', topic)
+        db.execute('update subs set notified = (?) where topicId = (?)', 0, topic)
         db.commit()
         
