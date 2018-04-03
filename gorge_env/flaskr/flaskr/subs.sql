50747a81b0b63bc9a8b697546a55c2f5afb84f59
George Neonakis <gnan88@mun.ca>
2018-04-03 11:49:15 -0230

    completed deliverable 2: requirements and supplementary materials

--- gorge_env/flaskr/flaskr/subs.sql
+++
@@ -1,7 +0,0 @@
-drop table if exists subscriptions;
-create table subscriptions (
-	id integer primary key autoincrement,
-	userid integer not null,
-	postid integer not null,
-	viewed boolean not null,
-);
